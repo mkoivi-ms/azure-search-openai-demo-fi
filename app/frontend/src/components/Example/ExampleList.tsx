2aca97c41a0ef44d40411d8ec4944fb456b738b2
Marko Koivisto <marko.kooo@gmail.com>
2023-05-31 17:08:54 +0300

    Fi translation added with Käypä hoito-suositukset content

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Lapsi on allerginen timoteille, onko siihen olemassa siedätyshoitoa?",
+        value: "Lapsi on allerginen timoteille, onko siihen olemassa siedätyshoitoa?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Epilapsiakohtaus on kestänyt 20 minuuttia, mitä riskejä siihen liittyy?", value: "Epilapsiakohtaus on kestänyt 20 minuuttia, mitä riskejä siihen liittyy?" },
+    { text: "Mies juo viikossa 30 tuopillista olutta, onko se liikaa?", value: "Mies juo viikossa 30 tuopillista olutta, onko se liikaa?" }
 ];
 
 interface Props {
--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -135,8 +135,10 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            <h1 className={styles.chatEmptyStateTitle}>OpenAI terveydenhuollossa</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Kysy tai keskustele Käypä hoito-suosituksen aiheista:</h2>
+                            <h6 className={styles.chatEmptyStateSubtitle}>Itselääkitys, keuhkoahtaumatauti, keuhkosyöpä, siedätyshoito, niskakipu, kohdunulkoinen raskaus, päänsärky, migreeni, alkoholiongelmaisen hoito, epileptinen kohtaus, lonkkamurtuma, diapeettinen retinopatia, ADHD, liikunta, atooppinen ekseema, lasten alahengitystieinfektiot, syömishäiriöt, kipu, sydämen vajaatoiminta, hampaan paikkaushoito </h6>
+
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
@@ -181,7 +183,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Kirjoita kysymys (esim Miten hoidetaan lonkkamurtumaa?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
